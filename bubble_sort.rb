c9ad8599501838c83e299c2d859f01a76d2210e6
Syouhei Tanaka <shou.tanak@gmail.com>
2014-02-18 11:00:19 +0900

    コメント足しただけ

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,11 +1,13 @@
+# バブルソートを実装
+
 class Array
 
   # バブルソート
   def bubble_sort
 
-    # 左端
+    # 左端 (インクリメント)
     1.upto(self.size-1) do |last|
-      # 右端
+      # 右端(左端と等しくなるまでデクリメント)
       (self.size-1).downto(last) do |start|
         # 隣り合う２つの要素を見比べて左側の要素が大きければswap!を呼ぶ
         self.swap!(start, start-1) if self[start] < self[start-1]
@@ -15,7 +17,7 @@ class Array
     self
   end
 
-  # 配列の要素を2つ指定して入れ替える
+  # 配列の要素を2つ指定してそれらを入れ替える
   def swap!(idx1, idx2)
     temp = self[idx1]
     self[idx1] = self[idx2]
@@ -26,4 +28,5 @@ class Array
 
 end
 
+# 実行
 puts [5, 7, 3, 4, 2, 8, 1, 6].bubble_sort
