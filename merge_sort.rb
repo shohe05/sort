63e42a6ba0c3c68886b61bf73dcbb7969f378eb2
Syouhei Tanaka <shou.tanak@gmail.com>
2014-02-24 23:43:41 +0900

    refactor merge_sort.rb

--- merge_sort.rb
+++ merge_sort.rb
@@ -3,9 +3,7 @@ require "./bubble_sort"
 
 class Array
   def merge_sort(merge = false)
-    if size <= 2
-      return self.bubble_sort!
-    end
+    return bubble_sort! if size <= 2
 
     ((self[0..size / 2 - 1].merge_sort).bubble_sort! + (self[size / 2..size - 1].merge_sort).bubble_sort!).bubble_sort!
   end
