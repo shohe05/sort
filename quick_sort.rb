6d549826441008d46f903ef5b1348d7e34064734
Syouhei Tanaka <shou.tanak@gmail.com>
2014-02-25 08:35:28 +0900

    write second quick sort

--- quick_sort.rb
+++ quick_sort.rb
@@ -13,11 +13,36 @@ class Array
 
     smallers.quick_sort + biggers.quick_sort
   end
+
+  def quicksort(left = 0, right = self.size - 1)
+    return self if left >= right
+
+    # 枢軸
+    pivot = self[((left + right).to_f / 2.to_f).ceil]
+    i = left
+    j = right
+
+    loop do
+      # 左側から見て枢軸より大きい値、右から見て枢軸より小さい値を見つける
+      i += 1 while self[i] < pivot
+      j -= 1 while self[j] > pivot
+
+      # 左側と右側がぶつかったら抜ける
+      break if i >= j
+
+      self[i], self[j] = self[j], self[i]
+      i += 1
+      j -= 1
+    end
+
+    quicksort(left, i - 1)
+    quicksort(i, right)
+  end
 end
 
 # 実行
 start_time = Time.now
-p [0, 5, 9, 2, 10, 1, 3, 4, 7, 8, 6].quick_sort
+p [3, 5, 4, 2, 1].quicksort
 end_time = Time.now
 
 p "#{end_time - start_time} second"
