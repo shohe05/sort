24e2d8559ab0bae32fa63a67bd3a44183e031314
Syouhei Tanaka <shou.tanak@gmail.com>
2014-02-25 12:58:05 +0900

    modify merge_sort.rb

--- merge_sort.rb
+++ merge_sort.rb
@@ -1,13 +1,37 @@
 # マージソートを実装
-require "./bubble_sort"
-
 class Array
   def merge_sort
-    return bubble_sort! if size <= 2
+    return ascending_merge if size == 2
 
-    ((self[0..size / 2 - 1].merge_sort).bubble_sort! + (self[size / 2..size - 1].merge_sort).bubble_sort!).bubble_sort!
+    ((self[0..size / 2 - 1].merge_sort).ascending_merge + (self[size / 2..size - 1].merge_sort).ascending_merge).ascending_merge
+  end
+
+  # 昇順にマージ
+  def ascending_merge
+    # 中央値をとる
+    pivot = (size.to_f / 2.to_f).ceil
+
+    # 中央値をもとに左右に分割
+    left = self[0...pivot]
+    right = self[pivot..size-1]
+
+    # 左右の配列をマージ
+    merged = []
+    while left.size > 0 and right.size > 0
+      case left[0] < right[0]
+      when true then merged << left.shift
+      else merged << right.shift
+      end
+    end
+
+    case left.empty?
+      when true then right[0...right.size].each {|e| merged << e}
+      else left[0...left.size].each {|e| merged << e}
+    end
+
+    merged
   end
 end
 
 # 実行
-p [5, 8, 4, 3, 7, 1, 2, 6].merge_sort
+p [8, 5, 4, 3, 7, 1, 2, 6].merge_sort
