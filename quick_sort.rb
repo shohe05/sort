253618daae6cd05f6990ee8d708182f4dcae8ccb
Syouhei Tanaka <shou.tanak@gmail.com>
2014-04-11 01:05:46 +0900

    delete unneeded code

--- quick_sort.rb
+++ quick_sort.rb
@@ -2,7 +2,6 @@
 # 真ん中を基準値とする
 
 class Array
-  # 自分で考えた方
   def quick_sort
     # ソートが完了していたら終了
     return self if empty? or zip(self[1...size] << max).map {|e| e[0] == e.min}.all?
@@ -14,37 +13,7 @@ class Array
 
     smallers.quick_sort + biggers.quick_sort
   end
-
-  # Wikipedia見て書いた
-  def quicksort(left = 0, right = self.size - 1)
-    return self if left >= right
-
-    # 枢軸
-    pivot = self[((left + right).to_f / 2.to_f).ceil]
-    i = left
-    j = right
-
-    loop do
-      # 左側から見て枢軸より大きい値、右から見て枢軸より小さい値を見つける
-      i += 1 while self[i] < pivot
-      j -= 1 while self[j] > pivot
-
-      # 左側と右側がぶつかったら抜ける
-      break if i >= j
-
-      self[i], self[j] = self[j], self[i]
-      i += 1
-      j -= 1
-    end
-
-    quicksort(left, i - 1)
-    quicksort(i, right)
-  end
 end
 
 # 実行
-start_time = Time.now
 p [3, 5, 4, 2, 1].quicksort
-end_time = Time.now
-
-p "#{end_time - start_time} second"
